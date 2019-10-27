1a4a3793a8e0a71753d93d183989d64d2fd4e3e6
demiislam <ferdous.islam@northsouth.edu>
2019-10-27 22:50:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.practiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button connectbutton;
+    TextView messagetextbox;
+    TextView wifitext;
+    TextView peertext;
+    Button sendbutton;
+
+    WifiP2pManager manager;
+    WifiP2pManager.Channel channel;
+    BroadcastReceiver receiver;
+    IntentFilter intentFilter;
+
+    WifiManager wifiManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        connectbutton = findViewById(R.id.connectbutton);
+        messagetextbox = findViewById(R.id.messagetextbox);
+        sendbutton = findViewById(R.id.sendbutton);
+
+        wifitext = findViewById(R.id.wifistatustext);
+        peertext = findViewById(R.id.peerstatustext);
+        initializeWifi();
+
+        if(wifiManager.isWifiEnabled()){
+            wifitext.setText("WiFi: ON");
+            connectbutton.setEnabled(true);
+        }
+
+        peerDiscovery();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+
+    private void initializeWifi() {
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+
+        //receive broadcasts from wifip2p
+        receiver = new WifiBroadcastReceiver(manager, channel, this);
+
+        // fix which intents to receive by WifiBroadCastReceiver
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+    }
+
+    private void peerDiscovery() {
+
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d("debugwifi", "onSuccess: peer discovery successfull");
+
+                messagetextbox.setEnabled(true);
+                sendbutton.setEnabled(true);
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Log.d("debugwifi", "onSuccess: peer discovery failed (fail code-"+reason+")");
+            }
+        });
+
+    }
+
+
+    public void wifiClick(View view) {
+
+        if(wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(false);
+            wifitext.setText("WiFi: OFF");
+
+            peertext.setText("Peer: none");
+            connectbutton.setEnabled(false);
+        }
+        else {
+            wifiManager.setWifiEnabled(true);
+            wifitext.setText("WiFi: ON");
+
+            connectbutton.setEnabled(true);
+        }
+    }
+
+
+    public void connectClick(View view) {
+
+        if( true ) {
+
+            peerDiscovery();
+
+            /*messagetextbox.setEnabled(true);
+            sendbutton.setEnabled(true);*/
+        }
+    }
+
+
+    public void sendClick(View view) {
+
+        //TODO: send data via wifi
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/practiceapp/WifiBroadcastReceiver.java
@@ -0,0 +1,98 @@
+package com.example.practiceapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WifiBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private MainActivity activity;
+
+    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+    private WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+            //Log.d("debugwifi", "onPeersAvailable: do notihng " + peerList.getDeviceList().toString());
+
+            List<WifiP2pDevice> newPeers = new ArrayList(peerList.getDeviceList());
+
+            if(!newPeers.equals(peers)){
+                peers.clear();
+                peers.addAll(newPeers);
+
+                activity.peertext.setText("Peers: "+peers.size());
+                Log.d("debugwifi", "onPeersAvailable: peers list updated - "+peers.toString());
+            }
+
+            if(peers.size()==0)
+                Log.d("debugwifi", "onPeersAvailable: no peers found");
+        }
+    };
+
+    public WifiBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
+
+        super();
+
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    public List<WifiP2pDevice> getPeers() {
+        return peers;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+
+        if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
+            // check if wifi p2p mode is enabled or disabled
+
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+
+            if(state == WifiP2pManager.WIFI_P2P_STATE_ENABLED){
+                Log.d("debugwifi", "onReceive: wifi is On ");
+            }
+            else {
+                Log.d("debugwifi", "onReceive: wifi is Off ");
+            }
+
+        }
+
+        else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+            Log.d("debugwifi", "onReceive: peer list changed ");
+
+            // request for peers
+            if(manager!=null)
+                manager.requestPeers(channel,peerListListener);
+        }
+
+        else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+            Log.d("debugwifi", "onReceive: peer connected/disconnected ");
+        }
+
+        else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+            Log.d("debugwifi", "onReceive: my device wifi status changed ");
+        }
+
+    }
+
+
+
+
+}
